2a86cf183d27372185027d78b0d3aa4f74395655
Sergey <sfrok99@gmail.com>
2019-04-20 16:55:53 +0300

    add log and refactoring

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import settings
+import logging
+import sys
+
+
+logging.basicConfig(format='[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
+                    stream=sys.stdout,
+                    level=logging.INFO,
+                    # filename='bot.log',
+                    filemode="w"
+                    )
 
 
 def start_bot(bot, update):
@@ -29,22 +39,25 @@ def get_list():
     return sorted(lst)
 
 
-# Отображение списка WIP
-def display_list(bot, update):
+def callback(bot, update):
     query = update.callback_query
     if query.data == "2":
         tmp = '\n'.join(get_list())
+        logger.info('List create')
         bot.sendMessage(text=f"Here's the list of available subjects:\n{tmp}",
                         chat_id=query.message.chat_id, message_id=query.message.message_id)
+        logger.info('Send message')
 
 
 def main():
     upd = Updater(settings.API_TOKEN)
     upd.dispatcher.add_handler(CommandHandler('start', start_bot))
-    upd.dispatcher.add_handler(CallbackQueryHandler(display_list))
+    upd.dispatcher.add_handler(CallbackQueryHandler(callback))
     upd.start_polling()
     upd.idle()
 
 
 if __name__ == '__main__':
+    logger = logging.getLogger('PythonBot')
+    logger.info('Bot started')
     main()
