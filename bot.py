7a95598a50b4362f9e92dd44f9c3a079f4de7bfc
Sergey <sfrok99@gmail.com>
2019-06-19 10:29:07 +0300

    add greet module

--- bot.py
+++ bot.py
@@ -1,26 +1,12 @@
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, RegexHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from greet import start_bot
 import settings
 import logs
 import data
 
 
 logger = None
-
-
-def start_bot(bot, update):
-    user_name = update.message.chat.first_name
-    bot_name = bot.first_name
-
-    main_menu_markup = [
-        [InlineKeyboardButton(text='Show subjects', callback_data='subjects')],
-        [InlineKeyboardButton(text='View schedule', callback_data='schedule')]
-    ]
-
-    reply_main_menu = InlineKeyboardMarkup(main_menu_markup)
-    update.message.reply_text(f'''Hello {user_name}!
-My name is {bot_name} and I will help you getting track of your study schedule.
-''', reply_markup=reply_main_menu)
 
 
 def regex_handler(bot, update, groups, user_data):
