3b12f1bad4588830a8362fe26d1847d857721af2
Ситэрай <s.t.100.rus.version@gmail.com>
2019-07-01 00:00:27 +0300

    Major logger refactoring, small improvements to data usage

--- logs.py
+++ logs.py
@@ -1,7 +1,7 @@
 import logging
 
 
-logging.basicConfig(format='[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
+logging.basicConfig(format='[%(name)s] %(message)s',
                     # stream=sys.stdout,
                     level=logging.INFO,
                     filename='bot.log',
