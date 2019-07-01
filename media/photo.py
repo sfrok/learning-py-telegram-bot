e83bc2c458c605f27638fa4c07f4b51e0bc0e1d3
Sergey <sfrok99@gmail.com>
2019-07-01 21:49:05 +0300

    update .gitignore

--- media/photo.py
+++ media/photo.py
@@ -1,12 +1,11 @@
 from logs import logger
-import os
+from telegram.ext import ConversationHandler
 
 
 def user_photo(bot, update, user_data):
-    gen = [x for x in range(20)]
-    photo = bot.get_file(update.message.photo[-1].file_id)
-    list_dir = os.listdir("pyTelegram/static")
-    pre_download = photo.download("static/photo_number.jpg")
-    for file in list_dir:
-        if pre_download == file:
+    photo_id = update.message.photo[-1].file_id
+    photo = bot.get_file(photo_id)
+    photo.download(f"static/{photo_id}.jpg")
+    bot.send_photo(chat_id=update.message.chat.id, photo=open(f"static/{photo_id}.jpg", "rb"))
+    return ConversationHandler.END
 
