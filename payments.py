37a1dfd7fca80ab356c6e9b12aed24afd7325a4d
Sergey <sfrok99@gmail.com>
2019-07-07 12:09:34 +0300

    add loggers to payments.py

--- payments.py
+++ payments.py
@@ -1,5 +1,9 @@
 from telegram import LabeledPrice
 import settings
+import logs
+
+
+logger = logs.logger
 
 
 def payment_noshipping(bot, update):
@@ -12,19 +16,24 @@ def payment_noshipping(bot, update):
     currency = 'UAH'
     price = 50
     prices = [LabeledPrice('test', price * 100)]
+    logger.info(f'Initializing Invoice info: chat_id: {chat_id}, title: {title}, payload: {payload} '
+                f'description: {description}, start_parameter: {start_parameter}, currency: {currency}')
     bot.send_invoice(chat_id, title, description, payload, provider_token, start_parameter, currency, prices)
 
 
 def precheckout_callback(bot, update):
     query = update.pre_checkout_query
+    logger.info(f'Stage: Callback "{query}"')
+    logger.info(f'Checking the equal stage of payload: {query.invoice_payload}')
     if query.invoice_payload != 'payment in process':
-        bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=False, error_message='Something went wrong')
+        bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=False, error_message='Something went wrong...')
     else:
+        logger.info(f'Checking the equal stage of payload: {query.invoice_payload}')
         bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True)
-        print(update)
+
 
 # ------------------------------------------------------------------------
 # def successful_payment_message(bot, update):
 #     update.message.reply_text(f'Thank u {update.message.first_name}')
 # ------------------------------------------------------------------------
-# THIS WORKING FOR REAL PAYMENT
+# THIS WORKING FOR REAL PAYMENT, CUZ HE SHOULD BE TO CONNECTING WITH PROVIDER
