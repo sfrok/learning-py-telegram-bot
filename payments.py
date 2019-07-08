9dad1849ae71d5aef36c1ba0527a2088327e5455
Ситэрай <s.t.100.rus.version@gmail.com>
2019-07-08 18:42:47 +0300

    Small refactoring, needs testing

--- payments.py
+++ payments.py
@@ -1,20 +1,36 @@
 from telegram import LabeledPrice, ShippingOption
-import settings
-import logs
+import data
 
-logger = logs.logger
+logger = data.logger
+
+# Indexing payment data, NO shipping
+NoShID = 'No Shipping Item #1' #start_parameter
+NoShLp = 'NoShLabeledPrice0'
+# Indexing payment data, shipping
+ShID = 'Shipping Item #1' #start_parameter
+ShLp = 'ShLabeledPrice0'
+ShOption = []
+ShOption.append('ShOption0Shipping')
+ShOption.append('ShOption1Shipping')
+ShLpShipping = []
+ShLpShipping.append('ShLabeledPrice0Shipping')
+ShLpShipping.append('ShLabeledPrice1Shipping')
+ShLpShipping.append('ShLabeledPrice2Shipping')
+# Indexing payment data, other
+ShPL = 'payload1' #payload
+UAH = 'UAH'
 
 
-def payment_shipping(bot, update):
+def shipping(bot, update):
     chat_id = update.message.chat_id
-    title = 'T-shirt with print'
-    payload = 'payment in process'
+    title = 'T-shirt'
+    payload = ShPL
     description = 'T-shirt with your print'
-    start_parameter = 'test-payment'
-    provider_token = settings.PAYMENT_TOKEN
-    currency = 'UAH'
+    start_parameter = ShID
+    provider_token = data.settings.PAYMENT_TOKEN
+    currency = UAH
     price = 10
-    prices = [LabeledPrice('test', price * 100)]
+    prices = [LabeledPrice(ShLp, price * 100)]
     logger.info(f'Initializing Invoice info: chat_id: {chat_id}, title: {title}, payload: {payload} '
                 f'description: {description}, start_parameter: {start_parameter}, currency: {currency}')
     bot.send_invoice(chat_id, title, description, payload, provider_token, start_parameter, currency, prices,
@@ -22,47 +38,45 @@ def payment_shipping(bot, update):
                      is_flexible=True)
 
 
-def payment_noshipping(bot, update):
+def shipping_callback(bot, update):
+    query = update.shipping_query
+    logger.info(f'Stage: Callback "shipping_callback", payload: "{query.invoice_payload}"')
+    if query.invoice_payload != ShPL:
+        bot.answer_shipping_query(shipping_query_id=query.id, ok=False, error_message='Error')
+        return
+    else:
+        options = []
+        options.append(ShippingOption(ShOption[0], 'Pickup Post №33', [LabeledPrice(ShLpShipping[0], 100)]))
+        price_list = [LabeledPrice(ShLpShipping[1], 150), LabeledPrice(ShLpShipping[2], 200)]
+        options.append(ShippingOption(ShOption[1], 'Delivery Post №22', price_list))
+        bot.answer_shipping_query(shipping_query_id=query.id, ok=True, shipping_options=options)
+
+
+def noshipping(bot, update):
     chat_id = update.message.chat_id
     title = 'Edit subject'
-    payload = 'payment in process'
-    description = 'This function will be to use button of edit subject in you list'
-    start_parameter = 'test-payment'
-    provider_token = settings.PAYMENT_TOKEN
-    currency = 'UAH'
+    payload = ShPL
+    description = 'Unlocks "Edit Subject Function"'
+    start_parameter = NoShID
+    provider_token = data.settings.PAYMENT_TOKEN
+    currency = UAH
     price = 10
-    prices = [LabeledPrice('test', price * 100)]
+    prices = [LabeledPrice(NoShLp, price * 100)]
     logger.info(f'Initializing Invoice info: chat_id: {chat_id}, title: {title}, payload: {payload} '
                 f'description: {description}, start_parameter: {start_parameter}, currency: {currency}')
     bot.send_invoice(chat_id, title, description, payload, provider_token, start_parameter, currency, prices)
 
 
-def shipping_callback(bot, update):
-    query = update.shipping_query
-    logger.info(f'Information of Callback "{query}"')
-    if query.invoice_payload != 'payment in process':
-        bot.answer_shipping_query(shipping_query_id=query.id, ok=False, error_message='Error')
-        return
-    else:
-        options = list()
-        # a single LabeledPrice
-        options.append(ShippingOption('post_1', 'Pickup Post №33', [LabeledPrice('Pickup in UA', 100)]))
-
-        # an array of LabeledPrice objects
-        price_list = [LabeledPrice('Box of product', 150), LabeledPrice('Fast delivery (1-2 days) in UA', 200)]
-        options.append(ShippingOption('post_2', 'Delivery Post №22', price_list))
-        bot.answer_shipping_query(shipping_query_id=query.id, ok=True, shipping_options=options)
-
-
 # The Bot API must receive an answer within 10 seconds after the pre-checkout query was sent.
 def precheckout_callback(bot, update):
     query = update.pre_checkout_query
-    logger.info(f'Information of Callback "{query}"')
-    if query.invoice_payload != 'payment in process':
+    logger.info(f'Stage: Callback "shipping_callback", payload: "{query.invoice_payload}"')
+    if query.invoice_payload != ShPL:
         bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=False, error_message='Error')
     else:
         bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True)
 
 
 def successful_payment_message(bot, update):
-    update.message.reply_text(f'Thank you, {update.message.chat.first_name} your purchase was completed!')
+    logger.info(f'Stage: Payment succsesful')
+    update.message.reply_text(f'Thank you, {update.message.chat.first_name}, your purchase was completed!')
