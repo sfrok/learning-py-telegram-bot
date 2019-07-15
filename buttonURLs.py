8228329a294649aef15b7944edc443ebea2b0257
Ситэрай <s.t.100.rus.version@gmail.com>
2019-07-15 11:40:47 +0300

    Added contact button, added decorator to media.py

--- buttonURLs.py
+++ buttonURLs.py
@@ -4,10 +4,11 @@ import data
 
 def linked_button(function):
     def wrapper(bot, update):
-        message_text = 'Enter the any button:'
+        message_text = 'Press any button:'
         markup = [
-            [InlineKeyboardButton(text='Go to google.com', url='https://www.google.com/', callback_data=data.cbSite)],
-            [InlineKeyboardButton(text='Go to our telegram channel', url='https://t.me/ithumor', callback_data=data.cbChannel)],
+            [InlineKeyboardButton(text='Go to google.com', url='https://www.google.com/')],
+            [InlineKeyboardButton(text='Go to our telegram channel', url='https://t.me/ithumor')],
+            [InlineKeyboardButton(text='Go to our telegram king', url='https://t.me/thecete')],
             [InlineKeyboardButton(text='Back', callback_data=data.cbMain)]
         ]
         reply = InlineKeyboardMarkup(markup)
